b528eb70bc41ac42bb340dcc99302547aa15196b
huseyn1211 <quluzadeh43@gmail.com>
2024-12-14 22:00:44 +0400

    class

--- app.js
+++ app.js
@@ -1,19 +1,74 @@
-let person = {
-    age:20,
-   setAge(){
-    return this.age += 5
-   },
+// class Person {
+//     constructor(name,age){
+//         this.name = name;
+//         this.age = age
 
-   newAge(){
-    return 65 -this.age
+//     }
+//     celebrateBirthday(){
+//         this.age +=1
+//     }
+// }
+// let b = new Person("hesen",15)
+// b.celebrateBirthday()
 
+// console.log(b)
+// let a = "salam menim cavanligimin ogrusu"
+// function myFunc(a){
+//     return a.split("").reverse().join("")
 
-   }
+// }
+// console.log(myFunc(a))
+// let a = [1,2,3,4,5,6,7,8,9]
+// function myFunc(a){
+//     let sum = 0
+//     for(i=0;i<a.length;i++){
+//         if(a[i] % 2 ===1){
+//             sum+=a[i]
+//         }
+        
+//     }
+//     return sum
+// }
+
+// console.log(myFunc(a))
+class Person {
+    constructor(surname,age,name){
+        this.surname = surname;
+        this.age = age;
+        this.name = name
+    }
+    changeName(){
+        this.name = "selim"
+    }
 }
- person.setAge()
-console.log(person.age),
+
+class Admin extends Person{
+    constructor(surname,age,name,role,salary){
+        super(surname,age,name);
+        this.role = role;
+        this.salary = salary
+
+    }
+}
+class Player extends Admin{
+    constructor(surname,age,name,role,salary,id){
+        super(surname,age,name,role,salary);
+        this.id = id
+    }
+
+}
+
+let a = new Person("quluzade",26,"huseyn")
+a.changeName()
+console.log(a)
 
 
-person.newAge()
-console.log(person.newAge())
+let b = new Admin('quluzade',26,'huseyn',"salam","1600manat")
+b.changeName()
+console.log(b)
 
+
+
+let c = new Player("hesimov",33,"aga","main","1300","1212")
+c.changeName()
+console.log(c)
